e8aa022ae399ef556db57f1ec548f004582af297
Dolph <dolphik@gmail.com>
2018-03-16 12:56:30 +0100

    Data features

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
 10.times do |blog|
   Blog.create!(
     title: "My blog post #{blog}",
-    body: "This is my blog post number #{blog}"
+    body: "This is my blog post number #{blog}",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +23,20 @@ end
 
 puts "5 skills created"
 
-9.times do |porfolio_item|
+8.times do |porfolio_item|
   Portfolio.create!(
     title: "Rails #{porfolio_item}",
-    subtitle: "this is subtitle #{porfolio_item}",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |porfolio_item|
+  Portfolio.create!(
+    title: "Angular #{porfolio_item}",
+    subtitle: "Angular",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/350x200"
@@ -27,3 +44,11 @@ puts "5 skills created"
 end
 
 puts "9 porfolio_items created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies_items created"
