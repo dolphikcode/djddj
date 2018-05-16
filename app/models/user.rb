72f5b28a15469d41c9b4ca3e723ba6af536b62d2
Dolph <dolphik@gmail.com>
2018-05-16 17:51:26 +0200

    Implemented authorization engine for portfolio and blogs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   ## The :user role is added by default and shouldn't be included in this list.             ##
   ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
   ## The multiple option can be set to true if you need users to have multiple roles.       ##
-  petergate(roles: [:admin, :editor], multiple: false)                                      ##
-  ############################################################################################ 
- 
+  petergate(roles: [:site_admin, :editor], multiple: false)                                      ##
+  ############################################################################################
+
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :timeoutable and :omniauthable:registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable, :timeoutable
 
   validates_presence_of :name
@@ -21,5 +21,13 @@ class User < ApplicationRecord
 
   def last_name
     self.name.split.last
+  end
+
+  def creat_usr
+    guest = User.new
+    guest.name = "AAAGuest User"
+    guest.email = "a@example.com"
+    guest.password = "ABCkosmos"
+    guest.save
   end
 end
